a1b17f0bb795daa1dcc4abab46a27551a5ed515c
Alex <alexandra.nguyen@epitech.eu>
2021-02-22 10:17:24 +0100

    decode cookie in context

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,58 +1,62 @@
 import { RemoteGraphQLDataSource } from '@apollo/gateway';
 import { Module } from '@nestjs/common';
 import { GATEWAY_BUILD_SERVICE, GraphQLGatewayModule } from '@nestjs/graphql';
-import {
-  GraphQLResponse
-} from 'apollo-server-types';
+import { ApolloError, AuthenticationError, ForbiddenError } from 'apollo-server-express';
+import { GraphQLResponse } from 'apollo-server-types';
+import * as admin from 'firebase-admin';
+
 class AuthenticatedDataSource extends RemoteGraphQLDataSource {
   // modify your gateway's requests to the implementing service before they're sent
   async willSendRequest({ request, context }) {
-    console.log('------ willSendRequest request.http.headers', request.http.headers)
     // console.log('------ willSendRequest context?.req?.cookies', context?.req?.cookies) // cookies are set
     // console.log('------ willSendRequest context.req.headers', context.req?.headers) // session cookie not set, (appears in browser if refresh)
-    console.log('------ willSendRequest request.http.cookies', request.http.headers)
-
-    // const { userId } = await decode(context.jwt);
-    // request.http.headers.set('Access-Control-Allow-Origin', context.req?.headers.origin);
-    // request.http.headers.set('x-user-id', 'hello');
-    // if (context.jwt) request.http.headers.set('session-cookie', context.jwt)
-    // check req.signedCookies
+    console.log('------ willSendRequest context.jwt', context?.jwt)
+    // const session = context?.session;
+    // if (session) {
+    //   admin
+    //     .auth()
+    //     .verifySessionCookie(session, true /** checkRevoked */)
+    //     .then((decodedClaims) => {
+    //       // request.http.headers.set('Authorization', session)
+    //       console.log({decodedClaims})
+    //       request.http.headers.set('x-user-id', decodedClaims)
+    //     })
+    //     .catch((error) => {
+    //       new AuthenticationError(error)
+    //     });
+    // }
+    console.log('------ willSendRequest request.http.headers', request.http.headers)
   }
 
   // modify the implementing service's responses before the gateway passes them along to the requesting client
   async didReceiveResponse({ request, response, context }): Promise<GraphQLResponse> {
-    // console.log('++++++ didResponse response.http.headers', response.http.headers)
-    // request.http.headers.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+    // response.http.headers.set('Access-Control-Allow-Origin', 'http://localhost:3000')
+    console.log('++++++ didResponse response.http.headers', response.http.headers) // origin set from users cors origin
 
     // set response header origin to the request origin (CORS errors)
     context?.res?.set('Access-Control-Allow-Origin', context.req?.headers.origin)
     // append the cookie to the response to see it in the browser
     const sessionCookie = response.http.headers.get('set-cookie')
     if (sessionCookie) context.res.append('set-cookie', sessionCookie);
+    // console.log('++++++ didResponse context?.res', context?.res)
 
     return response;
   }
 
+  async errorFromResponse(response) {
+    const message = `${response.status}: ${response.statusText}`;
 
-  //TODO: handle errors
-  // didEncounterError(error, request) {
-  //   throw error;
-  // }
+    let error: ApolloError;
+    if (response.status === 401) {
+      error = new AuthenticationError(message);
+    } else if (response.status === 403) {
+      error = new ForbiddenError(message);
+    } else {
+      error = new ApolloError(message);
+    }
 
-  // async errorFromResponse(response) {
-  //   const message = `${response.status}: ${response.statusText}`;
-
-  //   let error: ApolloError;
-  //   if (response.status === 401) {
-  //     error = new AuthenticationError(message);
-  //   } else if (response.status === 403) {
-  //     error = new ForbiddenError(message);
-  //   } else {
-  //     error = new ApolloError(message);
-  //   }
-
-  //   return error;
-  // }
+    return error;
+  }
 }
 
 @Module({
@@ -86,9 +90,6 @@ class BuildServiceModule {}
           serviceList: [
             { name: 'users', url: 'http://localhost:4000/graphql' },
             // { name: 'posts', url: 'http://localhost:4001/graphql' },
-
-            // { name: 'users', url: 'http://user-service/graphql' },
-            // { name: 'posts', url: 'http://post-service/graphql' },
           ],
         },
         server: {
@@ -97,12 +98,30 @@ class BuildServiceModule {}
           //   jwt: req.signedCookies?.['session-cookie'],
           //   cors: true,
           // })
-          // context: ({ req, res }) => {
-          //   console.log('context headers', req.headers)
-          //   console.log('context signedcookie', req.signedCookies)
-          //   console.log('context user', req.user)
-          // }
-          context: ({ req, res }) => ({ req, res })
+          context: async ({ req, res }) => {
+            // console.log('%%%%% context')
+            const session = req.cookies?.['session-cookie']
+            console.log('req.cookies', req.cookies)
+            let decodedClaims: admin.auth.DecodedIdToken
+            let userId: string;
+            if (session) {
+              try {
+                decodedClaims = await admin
+                  .auth()
+                  .verifySessionCookie(session, true /** checkRevoked */)
+                console.log({decodedClaims})
+                console.log('admin.auth().getUser(decodedClaims.uid)', await admin.auth().getUser(decodedClaims.uid))
+              } catch (error) {
+                new AuthenticationError(error)
+              }
+            }
+
+            return {
+              req,
+              res,
+              userId: decodedClaims?.uid
+            }
+          }
         },
       }),
       imports: [BuildServiceModule],
@@ -110,22 +129,5 @@ class BuildServiceModule {}
     }),
   ],
 })
-
-  // @Module({
-  //   imports: [
-  //     GraphQLGatewayModule.forRoot({
-  //       server: {
-  //         // ... Apollo server options
-  //         cors: true,
-  //       },
-  //       gateway: {
-  //         serviceList: [
-  //           { name: 'users', url: 'http://localhost:4000/graphql' },
-  //           { name: 'posts', url: 'http://localhost:4001/graphql' },
-  //         ],
-  //       },
-  //     }),
-  //   ],
-  // })
 
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import * as cookieParser from 'cookie-parser';
+import 'dotenv/config';
+import * as admin from 'firebase-admin';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // app.use(
-    //   expressJwt({
-      //     secret: "SNK",
-      //     algorithms: ["HS256"],
-      //     credentialsRequired: false
-      //   })
-      // );
-      // app.enableCors()
-      app.use(cookieParser());
-      app.enableCors({
-        credentials: true,
-        origin: 'http://localhost:3000'
-      })
-      // app.enableCors({ origin: true })
-      // app.enableCors({
-      //   "origin": "*",
-      //   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-      //   "preflightContinue": false,
-      //   "optionsSuccessStatus": 204,
-      //   "credentials": true
-      // })
+  // firebase init
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    })
+  });
+  // authorize cookies to be send
+  app.enableCors({
+    credentials: true,
+    origin: 'http://localhost:3000'
+  })
+  app.use(cookieParser());
+
   await app.listen(3001);
 }
 bootstrap();
